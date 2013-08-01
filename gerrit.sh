c2c9b60b73675d67fc2f09c54b1da4c70c0ec52f
Kyle Laker <kalaker@teamblueridge.org>
2013-08-01 12:59:10 -0400

    More updates
    * Variables should be wrapped in braces {} so that they are separate
    from the rest of the stuff around them
    
    * Make more things configurable at the beginning, this anyone can
    redo the script to fit their own configuration
    
    * Don't keep the username in .ssh. That's for SSH stuff. Put it in a
    hidden directory for the person/team/organization that the script is
    for instead.
    
    * End cases with esac
    
    Change-Id: I71c595c5311d75be098fdfd79e6b8a95fa5aa160

--- gerrit.sh
+++ gerrit.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+#These need to be changed to match your project & configuration
 name="Team BlueRidge Gerrit"
-url="http://review.teamblueridge.org"
+site="review.teamblueridge.org"
+url="http://${site}/"
 project="gerrit_script"
 port="29418" #Keep this the default unless you know to do otherwise
-branch="$1"
-unfile="~/.ssh/tbr_username"
+undir="$HOME/.tbr/"
+unfile="${undir}gerrit_username"
+un=`cat $unfile`
+branch="$1" #Don't change this
 clear
+function commit-check {
+	while true; do
+	read -p "Have you committed your changes? [Y/N] " yn
+	case $yn in
+	[Yy]* ) echo "Good. Let's push the change."; push-verify;;
+	[Nn]* ) commit;;
+	    * ) echo "Please answer Yes or No.";;
+	esac
+	done
+}
 function commit {
 	while true; do
 	read -p "Do you wish to commit all changes? [Y/N] " yn
 	case $yn in
 	[Yy]* ) echo "Good, let's continue"; git commit -a; break;;
-	[Nn]* ) echo "Please commit your changes manually."; exit;;
+	[Nn]* ) echo "Please commit your changes manually."; echo "Script exiting..."; exit;;
 	    * ) echo "Please answer Yes or No.";;
+	esac
 	done
 }
 function push-verify {
@@ -29,11 +44,12 @@ function push-verify {
 	[Yy]* ) echo "Good, let's continue"; do-push; break;;
 	[Nn]* ) echo "Please start over. Exiting"; exit;;
 	    * ) echo "Please answer Yes or No.";;
+	esac
 	done
 }
 function do-push {
 	echo "Preparing to push..."
-	git push "ssh://${un}@review.simonsickle.com:${port}/${project}.git HEAD:refs/for/$branch"
+	git push "ssh://${un}@${site}:${port}/${project}.git HEAD:refs/for/$branch"
 	if [ $? -eq 0 ]
 	then
 	echo "Done."
@@ -50,21 +66,21 @@ function username-check {
 	fi
 }
 function username-exist {
-	echo "Welcome back to $name, $un!"
+	echo "Welcome back to ${name}, ${un}!"
 	echo "You've already been through setup already so let's continue!"
 	echo "Your username is ${un}. To change this, execute 'rm -rf $unfile'."
 }
 function username-create {
-	echo "Welcome to $name"
+	echo "Welcome to ${name}"
 	echo "You need to setup your account."
-	echo "If you don't have a $name account, please go to $url and create one."
-	echo -n "What is your $name username: "
+	echo "If you don't have a ${name} account, please go to ${url} and create one."
+	echo -n "What is your ${name} username: "
 	read un
-	mkdir -p $HOME/.ssh/ > /dev/null 2>&1
-	echo $un > $unfile
-	echo "Your username is ${un}. To change this, execute 'rm -rf $unfile'."
+	mkdir -p ${undir} > /dev/null 2>&1
+	echo ${un} > ${unfile}
+	echo "Your username is ${un}. To change this, execute 'rm -rf ${unfile}'."
 	echo "BE SURE YOUR SSH KEYS ARE MATCHED WITH GERRIT IN YOUR SETTINGS"
 	echo "You can verify your SSH keys at:"
-	echo "${url}#/settings/ssh-keys"
+	echo "${url}/#/settings/ssh-keys"
 }
-
+$1
